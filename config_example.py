30e4d98f4a01e4b4de912330b8be4afd6215048e
Andrew <ajc308@gmail.com>
2016-06-07 09:25:06 -0400

    Add example config file

---
+++ config_example.py
@@ -0,0 +1,13 @@
+#--------------------------------------------------------------------------------#
+# Rename this file to config.py and add your instance specific information below #
+#--------------------------------------------------------------------------------#
+
+# Required fields
+DB_SERVER = '<Host>\<Instance>'
+DB_USER = '<Username>'
+DB_PASSWORD = '<Password>'
+
+# Optional fields
+# Can be left blank (''), only used to pre-populate input boxes
+DEFAULT_DATABASE = '<DB Name>'
+DEFAULT_TABLE = '<Table Name>'
